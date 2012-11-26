96ed414b169419d2262ba3c75d308568728846c7
JRoTheGeek <jvrosenman@gmail.com>
2012-11-26 00:20:02 -0500

    Added some basic socket stuff. Going to sleep.

--- overlay.cpp
+++ overlay.cpp
@@ -9,3 +9,14 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+
+int main(int argc, char** argv) {
+	struct addrinfo known, *server;
+	memset(&known, 0, sizeof known);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags = AI_PASSIVE;
+	
+	
+	return 0;
+}
